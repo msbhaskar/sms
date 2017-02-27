54faf655487b3872288598cb74eff4e88d08d80b
bhaskarm <bhaskarm@microsoft.com>
2017-02-26 22:30:18 -0800

    adding theme

---
+++ StudentManagementSystem/Src/WebApp/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentManagementSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "User Dashboard";
+            return View();
+        }
+    }
+}
--- StudentManagementSystem/Src/WebApp/Controllers/HomeController.cs
+++ StudentManagementSystem/Src/WebApp/Controllers/HomeController.cs
@@ -2,11 +2,50 @@
 {
     using System.Web.Mvc;
 
-    [Authorize]
+    // [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            ViewBag.Title = "Home";
+            return this.View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About US";
+            return this.View();
+        }
+
+
+        public ActionResult Schools()
+        {
+            ViewBag.Title = "Schools";
+            return this.View();
+        }
+
+
+        public ActionResult Curriculum()
+        {
+            ViewBag.Title = "Curriculum";
+            return this.View();
+        }
+
+        public ActionResult Calendar()
+        {
+            ViewBag.Title = "Calendar";
+            return this.View();
+        }
+
+        public ActionResult Faq()
+        {
+            ViewBag.Title = "FAQ";
+            return this.View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact US";
             return this.View();
         }
     }
